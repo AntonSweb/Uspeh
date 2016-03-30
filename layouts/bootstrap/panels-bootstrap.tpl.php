73dd97787a77a717c71cb721123faff1a617e0de
Anton Salatskyi <salatskyi_8@ukr.net>
2016-03-30 16:41:22 +0300

    add global layout

--- layouts/bootstrap/panels-bootstrap.tpl.php
+++ layouts/bootstrap/panels-bootstrap.tpl.php
@@ -1,37 +1,31 @@
-<?php
-/**
- * @file
- * Template for a 2 column panel layout.
- *
- * This template provides a two column panel display layout, with
- * each column roughly equal in width.
- *
- * Variables:
- * - $id: An optional CSS id to use for the layout.
- * - $content: An array of content, each item in the array is keyed to one
- *   panel of the layout. This layout supports the following sections:
- *   - $content['left']: Content in the left column.
- *   - $content['right']: Content in the right column.
- */
-?>
 <div class="panel-display clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <?php if (!empty($content['first'])): ?>
   <div class="panel-panel panel-col-first">
     <div class="inside"><?php print $content['first']; ?></div>
   </div>
+  <?php endif; ?>
 
+  <?php if (!empty($content['second'])): ?>
   <div class="panel-panel panel-col-second">
     <div class="inside"><?php print $content['second']; ?></div>
   </div>
+  <?php endif; ?>
 
+  <?php if (!empty($content['third'])): ?>
   <div class="panel-panel panel-col-third">
     <div class="inside"><?php print $content['third']; ?></div>
   </div>
+  <?php endif; ?>
 
+  <?php if (!empty($content['fourth'])): ?>
   <div class="panel-panel panel-col-fourth">
     <div class="inside"><?php print $content['fourth']; ?></div>
   </div>
+  <?php endif; ?>
 
+  <?php if (!empty($content['fifth'])): ?>
   <div class="panel-panel panel-col-fifth">
     <div class="inside"><?php print $content['fifth']; ?></div>
   </div>
+  <?php endif; ?>
 </div>
